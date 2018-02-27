299d3c4eb775152a26720473c421b9c4a5f45fd2
Ivan Malison <IvanMalison@gmail.com>
2018-02-27 14:58:32 -0800

    Get xml parsing working

--- src/AppIndicator/Protocol.hs
+++ src/AppIndicator/Protocol.hs
@@ -1,1 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module AppIndicator.Protocol where
+
+import AppIndicator.Util
+import DBus
+import DBus.Client
+import DBus.Introspection
+import Data.List (sort)
+import Data.Maybe
+import Data.String (fromString)
+import Paths_appindicator_tray ( getDataDir )
+import System.FilePath ( (</>) )
+import System.IO.Unsafe
+
+getXMLDataFile :: String -> IO FilePath
+getXMLDataFile filename = (</> filename) . (</> "xml") <$> getDataDir
+
+statusNotifierWatcher = head . maybeToList .
+  parseXML (fromString "/") <$>
+  (getXMLDataFile "StatusNotifierWatcher.xml" >>= readFile)
---
+++ src/AppIndicator/Util.hs
@@ -0,0 +1,10 @@
+module AppIndicator.Util where
+
+infixl 4 ??
+(??) :: Functor f => f (a -> b) -> a -> f b
+fab ?? a = fmap ($ a) fab
+{-# INLINE (??) #-}
+
+ifM :: Monad m => m Bool -> m a -> m a -> m a
+ifM cond whenTrue whenFalse =
+  cond >>= (\bool -> if bool then whenTrue else whenFalse)
