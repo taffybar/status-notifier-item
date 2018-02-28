4c37771652da343b43b38cd74bd6d5b1e20e0208
Ivan Malison <IvanMalison@gmail.com>
2018-02-28 01:33:44 -0800

    Emit watcher signals

--- src/StatusNotifier/Watcher.hs
+++ src/StatusNotifier/Watcher.hs
@@ -5,6 +5,7 @@ import Control.Monad
 import Control.Concurrent.MVar
 import DBus
 import DBus.Client
+import DBus.Internal.Message
 import Data.Int
 import Data.List
 import Data.String
@@ -17,8 +18,17 @@ statusNotifierWatcherString = "StatusNotifierWatcher"
 watcherInterface :: InterfaceName
 watcherInterface = fromString $ printf "%s.%s" protocolPrefix statusNotifierWatcherString
 
-data ItemEntry = ItemEntry
-  { serviceName :: String
+makeWatcherMethod :: AutoMethod fn => MemberName -> fn -> Method
+makeWatcherMethod = autoMethod watcherInterface
+
+watcherSignal =
+  Signal
+  { signalPath = "/"
+  , signalInterface = watcherInterface
+  , signalMember = ""
+  , signalSender = Nothing
+  , signalDestination = Nothing
+  , signalBody = []
   }
 
 nameOwnerChangedMatchRule =
@@ -27,19 +37,31 @@ nameOwnerChangedMatchRule =
   , matchMember = Just "NameOwnerChanged"
   }
 
-startWatcherRequests = do
+data ItemEntry = ItemEntry
+  { serviceName :: String
+  }
+
+startWatcher = do
   client <- connectSession
   notifierItems <- newMVar []
   notifierHosts <- newMVar []
 
-  let registerStatusNotifierItem name =
+  let emitFromWatcher signal name =
+        emit client
+             watcherSignal { signalMember = signal
+                           , signalBody = [toVariant name]
+                           }
+
+      registerStatusNotifierItem name =
         modifyMVar_ notifierItems $ \currentItems ->
           do
+            emitFromWatcher "StatusNotifierItemRegistered" name
             return $ ItemEntry { serviceName = name } : currentItems
 
       registerStatusNotifierHost name =
         modifyMVar_ notifierHosts $ \currentHosts ->
           do
+            emit client watcherSignal { signalMember = "StatusNotifierHostRegistered" }
             return $ ItemEntry { serviceName = name } : currentHosts
 
       registeredStatusNotifierItems = map serviceName <$> readMVar notifierItems
@@ -47,9 +69,6 @@ startWatcherRequests = do
       isStatusNotifierHostRegistered = not . null <$> readMVar notifierHosts
 
       protocolVersion = return 1 :: IO Int32
-
-      makeMethod :: AutoMethod fn => MemberName -> fn -> Method
-      makeMethod = autoMethod watcherInterface
 
       filterDeadService deadService mvar =
         modifyMVar mvar $ return . partition ((/= deadService) . serviceName)
@@ -60,6 +79,8 @@ startWatcherRequests = do
             when (newOwner == "") $
                  do
                    removedItems <- filterDeadService name notifierItems
+                   when (not $ null removedItems) $
+                        emitFromWatcher "StatusNotifierItemUnregistered" name
                    removedHosts <- filterDeadService name notifierHosts
                    return ()
           _ -> return ()
@@ -67,9 +88,9 @@ startWatcherRequests = do
   _ <- requestName client "org.freedesktop.StatusNotifierWatcher" []
   _ <- addMatch client nameOwnerChangedMatchRule handleNameOwnerChanged
   export client "/"
-           [ makeMethod "RegisterStatusNotifierItem" registerStatusNotifierItem
-           , makeMethod "RegisterStatusNotifierHost" registerStatusNotifierHost
-           , makeMethod "RegisteredStatusNotifierItems" registeredStatusNotifierItems
-           , makeMethod "IsStatusNotifierHostRegistered" isStatusNotifierHostRegistered
-           , makeMethod "ProtocolVersion" protocolVersion
+           [ makeWatcherMethod "RegisterStatusNotifierItem" registerStatusNotifierItem
+           , makeWatcherMethod "RegisterStatusNotifierHost" registerStatusNotifierHost
+           , makeWatcherMethod "RegisteredStatusNotifierItems" registeredStatusNotifierItems
+           , makeWatcherMethod "IsStatusNotifierHostRegistered" isStatusNotifierHostRegistered
+           , makeWatcherMethod "ProtocolVersion" protocolVersion
            ]
