c60160d11a69851fc4cb645221017b58bee0be0c
Ivan Malison <IvanMalison@gmail.com>
2018-03-05 16:10:55 -0800

    Add command line arguments to watcher executable

--- WatcherMain.hs
+++ WatcherMain.hs
@@ -1,11 +1,36 @@
 module Main where
 
-import StatusNotifier.Watcher
 import Control.Concurrent.MVar
+import Data.Semigroup ((<>))
+import Options.Applicative
+import StatusNotifier.Watcher
+
+setWatcherParams namespace path =
+  defaultWatcherParams
+  { watcherNamespace = namespace
+  , watcherPath = path
+  }
+
+watcherParamsParser :: Parser WatcherParams
+watcherParamsParser = setWatcherParams
+  <$> strOption
+  (  long "namespace"
+  <> short 'n'
+  <> metavar "NAMESPACE"
+  <> value "org.kde"
+  <> help "The namespace the watcher should register at.")
+  <*> strOption
+  (  long "path"
+  <> short 'p'
+  <> metavar "DBUS-PATH"
+  <> value "/StatusNotifierWatcher"
+  <> help "The path at which to run the watcher." )
 
 main = do
+  watcherParams <- execParser $
+                   info (watcherParamsParser <**> helper)
+                   (  fullDesc
+                   <> progDesc "Run a StatusNotifierWatcher")
   stop <- newEmptyMVar
-  startWatcher defaultWatcherParams { watcherStop = putMVar stop ()
-                                    , watcherNamespace = "org.kde"
-                                    }
+  startWatcher watcherParams { watcherStop = putMVar stop () }
   takeMVar stop
