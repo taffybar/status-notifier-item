47a6cca68b11bcdd75f3803e8c63b879fbef5a9b
Ivan Malison <IvanMalison@gmail.com>
2018-03-20 21:29:51 -0700

    Don't use imported Item since it will result in file does not exist

--- src/StatusNotifier/Watcher/Service.hs
+++ src/StatusNotifier/Watcher/Service.hs
@@ -20,13 +20,13 @@ import           Data.List
 import           Data.Maybe
 import           Data.Monoid
 import           Data.String
-import qualified StatusNotifier.Item.Constants as Item
 import           StatusNotifier.Util
 import           StatusNotifier.Watcher.Constants
 import           StatusNotifier.Watcher.Signals
 import           System.IO.Unsafe
 import           System.Log.Logger
 import           Text.Printf
+
 
 buildWatcher WatcherParams
                { watcherNamespace = interfaceNamespace
@@ -57,7 +57,7 @@ buildWatcher WatcherParams
             parseServiceError = makeErrorReply errorInvalidParameters $
               printf "the provided service %s could not be parsed \
                      \as a bus name or an object path." name
-            path = fromMaybe Item.defaultPath $ T.parseObjectPath name
+            path = fromMaybe "/StatusNotifierItem"  $ T.parseObjectPath name
             remapErrorName =
               left $ (`makeErrorReply` "Failed to verify ownership.") .
                    M.methodErrorName
