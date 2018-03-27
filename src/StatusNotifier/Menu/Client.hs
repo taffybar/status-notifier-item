b4b9380d8558edac870f3e686bb6384991f51f0a
Ivan Malison <IvanMalison@gmail.com>
2018-03-27 08:29:33 -0700

    Remove menu code

--- src/StatusNotifier/Menu/Client.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module StatusNotifier.Menu.Client where
-
-import DBus.Generation
-import StatusNotifier.Menu.Constants
-
-generateClient defaultGenerationParams introspectionInterface
-generateSignalsFromInterface defaultGenerationParams introspectionInterface
